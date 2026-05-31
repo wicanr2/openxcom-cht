b729a87af706e2fe9f95255724b4cd0cf415d5cb
wicanr2 <wicanr2@users.noreply.github.com>
2026-05-31 18:06:46 +0800

    v2.19: 主選單作者簽名 chibi pixel art
    新增「原來是個胖仔 / wicanr2」作者簽名圖像於主選單右下角。
    
    設計（資深視覺設計師 agent）：
    - bin/common/Resources/signature_pang.png: 48x24 8bpp paletted PNG
    - 左半 24x24: chibi 圓臉戴大綠色眼鏡的胖仔頭像
    - 右半 24x24: WICANR2 (3x5 上) + 「胖仔」(12x12 手繪 下)
    - Palette: GEOSCAPE block 14 橄欖→暗棕漸層 + idx 32 亮綠 (眼鏡點綴)
    - 8 個 palette idx 全在 PAL_GEOSCAPE 範圍，跟 BACK01.SCR 自然融合
    - 4x scale 後 (1280x800 display) 顯示為 192x96 px，肉眼易辨
    
    整合 (patches/src/Menu/MainMenuState.{cpp,h}):
    - 加 Surface *_sigBadge 成員
    - 加 include Surface.h + FileMap.h
    - Constructor: new Surface(48,24,270,174) + loadImage()
    - add(_sigBadge) 用 raw surface overload (不走 interfaces.rul)
    - try/catch 包覆 loadImage，PNG 缺失 non-fatal
    
    驗證：
    - Windows EXE rebuild OK (3.72 MB, MSVC /utf-8)
    - F12 內建截圖 1280x800 確認簽名在右下角清楚渲染
    - 主選單按鈕（新遊戲/新戰/讀取存檔/選項/模組/離開）正常
    - screenshots/signature_main_menu.png
    
    Design notes 完整版在 docs/_ux_v2_tools/ 與 D:\openxcom\docs\signature_design.md

---
+++ patches/src/Menu/MainMenuState.cpp
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2010-2016 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "MainMenuState.h"
+#include <sstream>
+#include "../version.h"
+#include "../Engine/Game.h"
+#include "../Mod/Mod.h"
+#include "../Engine/LocalizedText.h"
+#include "../Engine/Screen.h"
+#include "../Engine/Surface.h"
+#include "../Engine/FileMap.h"
+#include "../Interface/TextButton.h"
+#include "../Interface/Window.h"
+#include "../Interface/Text.h"
+#include "NewGameState.h"
+#include "NewBattleState.h"
+#include "ListLoadState.h"
+#include "OptionsVideoState.h"
+#include "ModListState.h"
+#include "../Engine/Options.h"
+
+namespace OpenXcom
+{
+
+void GoToMainMenuState::init()
+{
+	Screen::updateScale(Options::geoscapeScale, Options::baseXGeoscape, Options::baseYGeoscape, true);
+	_game->getScreen()->resetDisplay(false);
+	_game->setState(new MainMenuState);
+}
+
+/**
+ * Initializes all the elements in the Main Menu window.
+ * @param game Pointer to the core game.
+ */
+MainMenuState::MainMenuState()
+{
+	// Create objects
+	_window = new Window(this, 256, 160, 32, 20, POPUP_BOTH);
+	_btnNewGame = new TextButton(92, 20, 64, 90);
+	_btnNewBattle = new TextButton(92, 20, 164, 90);
+	_btnLoad = new TextButton(92, 20, 64, 118);
+	_btnOptions = new TextButton(92, 20, 164, 118);
+	_btnMods = new TextButton(92, 20, 64, 146);
+	_btnQuit = new TextButton(92, 20, 164, 146);
+	_txtTitle = new Text(256, 30, 32, 45);
+
+	// zh-TW 簽名圖 (原來是個胖仔 / wicanr2)
+	// 48x24 paletted PNG，放右下角 BACK01.SCR 背景上不擋按鈕
+	// 設計：chibi 圓臉戴綠眼鏡 + "WICANR2" + 「胖仔」
+	// 用 GEOSCAPE palette block 14 (橄欖→暗棕) + idx 32 (亮綠眼鏡點綴)
+	_sigBadge = new Surface(48, 24, 270, 174);
+
+	// Set palette
+	setInterface("mainMenu");
+
+	add(_window, "window", "mainMenu");
+	add(_btnNewGame, "button", "mainMenu");
+	add(_btnNewBattle, "button", "mainMenu");
+	add(_btnLoad, "button", "mainMenu");
+	add(_btnOptions, "button", "mainMenu");
+	add(_btnMods, "button", "mainMenu");
+	add(_btnQuit, "button", "mainMenu");
+	add(_txtTitle, "text", "mainMenu");
+	add(_sigBadge);  // raw surface (uses parent state's palette)
+
+	// Load signature image from common/Resources/
+	try
+	{
+		_sigBadge->loadImage(FileMap::getFilePath("Resources/signature_pang.png"));
+	}
+	catch (...)
+	{
+		// missing PNG is non-fatal; signature just won't render
+	}
+
+	centerAllSurfaces();
+
+	// Set up objects
+	_window->setBackground(_game->getMod()->getSurface("BACK01.SCR"));
+
+	_btnNewGame->setText(tr("STR_NEW_GAME"));
+	_btnNewGame->onMouseClick((ActionHandler)&MainMenuState::btnNewGameClick);
+
+	_btnNewBattle->setText(tr("STR_NEW_BATTLE"));
+	_btnNewBattle->onMouseClick((ActionHandler)&MainMenuState::btnNewBattleClick);
+
+	_btnLoad->setText(tr("STR_LOAD_SAVED_GAME"));
+	_btnLoad->onMouseClick((ActionHandler)&MainMenuState::btnLoadClick);
+
+	_btnOptions->setText(tr("STR_OPTIONS"));
+	_btnOptions->onMouseClick((ActionHandler)&MainMenuState::btnOptionsClick);
+
+	_btnMods->setText(tr("STR_MODS"));
+	_btnMods->onMouseClick((ActionHandler)&MainMenuState::btnModsClick);
+
+	_btnQuit->setText(tr("STR_QUIT"));
+	_btnQuit->onMouseClick((ActionHandler)&MainMenuState::btnQuitClick);
+
+	_txtTitle->setAlign(ALIGN_CENTER);
+	_txtTitle->setBig();
+	std::ostringstream title;
+	title << tr("STR_OPENXCOM") << Unicode::TOK_NL_SMALL;
+	title << OPENXCOM_VERSION_SHORT << OPENXCOM_VERSION_GIT;
+	_txtTitle->setText(title.str());
+}
+
+/**
+ *
+ */
+MainMenuState::~MainMenuState()
+{
+
+}
+
+/**
+ * Opens the New Game window.
+ * @param action Pointer to an action.
+ */
+void MainMenuState::btnNewGameClick(Action *)
+{
+	_game->pushState(new NewGameState);
+}
+
+/**
+ * Opens the New Battle screen.
+ * @param action Pointer to an action.
+ */
+void MainMenuState::btnNewBattleClick(Action *)
+{
+	_game->pushState(new NewBattleState);
+}
+
+/**
+ * Opens the Load Game screen.
+ * @param action Pointer to an action.
+ */
+void MainMenuState::btnLoadClick(Action *)
+{
+	_game->pushState(new ListLoadState(OPT_MENU));
+}
+
+/**
+ * Opens the Options screen.
+ * @param action Pointer to an action.
+ */
+void MainMenuState::btnOptionsClick(Action *)
+{
+	Options::backupDisplay();
+	_game->pushState(new OptionsVideoState(OPT_MENU));
+}
+
+/**
+* Opens the Mods screen.
+* @param action Pointer to an action.
+*/
+void MainMenuState::btnModsClick(Action *)
+{
+	_game->pushState(new ModListState);
+}
+
+/**
+ * Quits the game.
+ * @param action Pointer to an action.
+ */
+void MainMenuState::btnQuitClick(Action *)
+{
+	_game->quit();
+}
+
+/**
+ * Updates the scale.
+ * @param dX delta of X;
+ * @param dY delta of Y;
+ */
+void MainMenuState::resize(int &dX, int &dY)
+{
+	dX = Options::baseXResolution;
+	dY = Options::baseYResolution;
+	Screen::updateScale(Options::geoscapeScale, Options::baseXGeoscape, Options::baseYGeoscape, true);
+	dX = Options::baseXResolution - dX;
+	dY = Options::baseYResolution - dY;
+	State::resize(dX, dY);
+}
+
+}
---
+++ patches/src/Menu/MainMenuState.h
@@ -0,0 +1,69 @@
+#pragma once
+/*
+ * Copyright 2010-2016 OpenXcom Developers.
+ *
+ * This file is part of OpenXcom.
+ *
+ * OpenXcom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenXcom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "../Engine/State.h"
+
+namespace OpenXcom
+{
+
+class TextButton;
+class Window;
+class Text;
+class Surface;
+
+// Utility class for enqueuing a state in the stack that goes to the main menu
+class GoToMainMenuState : public State
+{
+public:
+	void init();
+};
+
+/**
+ * Main Menu window displayed when first
+ * starting the game.
+ */
+class MainMenuState : public State
+{
+private:
+	TextButton *_btnNewGame, *_btnNewBattle, *_btnLoad, *_btnOptions, *_btnMods, *_btnQuit;
+	Window *_window;
+	Text *_txtTitle;
+	Surface *_sigBadge;  // zh-TW 簽名圖 (原來是個胖仔 / wicanr2)
+public:
+	/// Creates the Main Menu state.
+	MainMenuState();
+	/// Cleans up the Main Menu state.
+	~MainMenuState();
+	/// Handler for clicking the New Game button.
+	void btnNewGameClick(Action *action);
+	/// Handler for clicking the New Battle button.
+	void btnNewBattleClick(Action *action);
+	/// Handler for clicking the Load Saved Game button.
+	void btnLoadClick(Action *action);
+	/// Handler for clicking the Options button.
+	void btnOptionsClick(Action *action);
+	/// Handler for clicking the Mods button.
+	void btnModsClick(Action *action);
+	/// Handler for clicking the Quit button.
+	void btnQuitClick(Action *action);
+	/// Update the resolution settings, we just resized the window.
+	void resize(int &dX, int &dY);
+};
+
+}
